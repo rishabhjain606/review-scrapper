041f2b038caf2c5b82f061d25bd7f5c2898405ba
Rishabh <rishabh@eleve.co.in>
2021-08-16 00:24:05 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pre-reqs
+- Install [Node.js](https://nodejs.org/en/)
+
+node version above 10.19
+
+# Getting started
+- Clone the repository
+```
+git clone git@github.com:rishabhjain606/review-scrapper.git <project_name>
+```
+
+- Install dependencies
+```
+cd <project_name>
+npm install
+```
+- Build and run the project
+```
+npm start
+```
+
+- Navigate to `http://localhost:4000` to check if it is working
+
+- For fetching reviews of website  `http://localhost:4000/getReviews?reviewUrl=<URL>`
--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -4,7 +4,7 @@ import { Response, Request, NextFunction } from "express";
 import * as _ from "lodash";
 
 
-export const getTigerDirectReview= async(req: Request, res: Response, next: NextFunction) => {
+export const getTigerDirectReview = async (req: Request, res: Response, next: NextFunction) => {
     try {
         let url = req.query.reviewUrl ? req.query.reviewUrl.trim() : null;
         if (!_.isNil(url) && !_.isEmpty(url) && url.indexOf("https://www.tigerdirect.com/") > -1) {
@@ -20,40 +20,45 @@ export const getTigerDirectReview= async(req: Request, res: Response, next: Next
             } catch (err) {
                 console.log("Error in opening puppeteer");
                 res.status(500).send({ status: "fail", error_message: "Internal Server Error. Please contact the website owner" })
-
+                return
             }
-            const page = await browser.newPage()
-            page.setExtraHTTPHeaders({
-                'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-                'authority': 'www.tigerdirect.com'
-            });
-            page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36');
-            await page.goto(url)
+            if (!_.isNil(browser)) {
+                const page = await browser.newPage()
+                page.setExtraHTTPHeaders({
+                    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+                    'authority': 'www.tigerdirect.com'
+                });
+                page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36');
+                await page.goto(url)
 
-            const content = await page.content();
-            await browser.close()
-            const C = cheerio.load(content);
-            const $ = C;
-            let allReviews = $("#customerReviews>.review").get()
-            if (allReviews && allReviews.length > 0) {
-                let formattedReview = allReviews.map((o, i) => {
-                    let object = $("#customerReviews>.review").get()[i]
-                    let ratings = $(object).find(".itemRating>strong").text();
-                    let dates = $(object).find(".reviewer>dd").eq(1).text();
-                    let reviewerName = $(object).find(".reviewer>dd").eq(0).text();
-                    let title = $(object).find("blockquote>h6").text();
-                    let comment = $(object).find("blockquote>p").text();
-                    return {
-                        rating: ratings,
-                        date: dates,
-                        reviewerName: reviewerName.trim().slice(-1) == "," ? reviewerName.slice(0, -1) : reviewerName,
-                        title: title,
-                        comment: comment
-                    }
-                })
-                res.status(200).send({ status: "success", data: formattedReview, error_message: null })
-            } else {
-                res.status(404).send({ status: "success", data: [], error_message: null })
+                const content = await page.content();
+                await browser.close()
+                const C = cheerio.load(content);
+                const $ = C;
+                let allReviews = $("#customerReviews>.review").get()
+                if (allReviews && allReviews.length > 0) {
+                    let formattedReview = allReviews.map((o, i) => {
+                        let object = $("#customerReviews>.review").get()[i]
+                        let ratings = $(object).find(".itemRating>strong").text();
+                        let dates = $(object).find(".reviewer>dd").eq(1).text();
+                        let reviewerName = $(object).find(".reviewer>dd").eq(0).text();
+                        let title = $(object).find("blockquote>h6").text();
+                        let comment = $(object).find("blockquote>p").text();
+                        return {
+                            rating: ratings,
+                            date: dates,
+                            reviewerName: reviewerName.trim().slice(-1) == "," ? reviewerName.slice(0, -1) : reviewerName,
+                            title: title,
+                            comment: comment
+                        }
+                    })
+                    res.status(200).send({ status: "success", data: formattedReview, error_message: null })
+                } else {
+                    res.status(404).send({ status: "success", data: [], error_message: null })
+                }
+            }else{
+                res.status(500).send({ status: "fail", error_message: "Internal Server Error. Please contact the website owner" })
+                return
             }
         } else {
             res.status(400).send({ status: "fail", error_message: "Invalid URL" })
@@ -61,5 +66,6 @@ export const getTigerDirectReview= async(req: Request, res: Response, next: Next
     } catch (err) {
         console.log(err)
         res.status(500).send({ status: "fail", error_message: "Internal Server Error. Please contact the website owner" })
+        return
     }
 }
