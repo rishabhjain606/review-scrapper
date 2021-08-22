6aafb28d85ef4d86bbeefbd868fe78c6eb309d03
Rishabh <rishabh@eleve.co.in>
2021-08-23 01:13:36 +0530

    error handling and pagination

---
+++ src/constants/Constants.ts
@@ -0,0 +1,6 @@
+export const TigerHeaders = {
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'authority': 'www.tigerdirect.com'
+}
+
+export const UserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
